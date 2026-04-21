b72a6019321b48e36d5b4939eabdcd1d90360b98
Aham0803 <ahamgolash9340@gmail.com>
2026-04-21 22:46:05 +0530

    notes

--- Python Learning/3_numbers.py
+++ Python Learning/3_numbers.py
@@ -11,10 +11,17 @@ print(type(x))
 x = int(x)
 print(type(x))
 
+#complex(real , imag) -> creates a complex number using real and imaginary parts
 x = 3 #real
 y = 4 #img
 print(complex(x,y))
 
 #Math Operator
 print(7/2)
-print(7//2) #floor devision -> // 
+print(7//2) #floor devision -> it divides two numbers and rounds down  
+print(2 ** 3) #exponentiation -> it raises a number to the power of another number 
+
+#Rounding
+print(abs(2-10)) #abs(value) -> returns the absolute (non-negative) value of a number 
+
+
