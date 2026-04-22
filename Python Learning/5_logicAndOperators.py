8431906b431a9761614e6502bbb802293c63145c
Aham0803 <ahamgolash9340@gmail.com>
2026-04-22 18:02:49 +0530

    notes

---
+++ Python Learning/5_logicAndOperators.py
@@ -0,0 +1,10 @@
+email = ""
+phone = "01456664"
+userName = ""
+
+# any() -> returns true if one value is true
+print(any([email, phone, userName]))
+# all() -> returns true if all values are true
+print(all([email, phone, userName]))
+
+#logical operations
