55d4037b9e435b9acd4737354fe6fc60b28b3747
Aham0803 <ahamgolash9340@gmail.com>
2026-04-23 19:45:03 +0530

    notes

--- Python Learning/5_logicAndOperators.py
+++ Python Learning/5_logicAndOperators.py
@@ -9,3 +9,19 @@ print(all([email, phone, userName]))
 
 #logical operations
 print(not 3 > 2)
+
+# in operator -> checks if value inside another value , like a string , list , tuple or other sequence
+print('o' in 'python')
+print('z' not in 'python')
+print(3 in [1, 2 , 3])
+
+# is operator -> checks if two variabkes refer to same obj in memory
+x = ['a' , 'b' , 'c']
+y = ['a' , 'b' , 'c'] 
+print(x == y)
+print(x is y)
+
+x = 10
+y = 10
+print(x == y)
+print(x is y)
