409ba5b10dbf7e4c6888c69917ec3e0c306f77d5
Aham0803 <ahamgolash9340@gmail.com>
2026-04-26 13:09:41 +0530

    notes

--- Python Learning/8_Loops.py
+++ Python Learning/8_Loops.py
@@ -13,3 +13,30 @@ for item in items:
 # Range(start,stop,step)
 for item in range(5):
     print(f"Round : {item}")
+
+scores = [80, 50 , 60 , 75]
+total = 0
+for score in scores:
+    total += score
+    print("Current Total :" , total)
+print("Final Total :" , total)
+
+#ques -> clean the following data
+files = ['  Report.csv  ', 'Data.csv  ' , '  final.TXT']
+for file in files:
+    file = file.strip().lower().replace('.txt' , '.csv')
+    print(file)
+
+#ques -> print the 7-times table from 1 to 10 using for loop
+for table in range(1,11):
+    print(f'7 * {table} = {7*table}')
+
+#ques -> 
+# *
+# **
+# ***
+# ****
+# *****
+
+for start in range(1,6):
+    print("*" * start)
