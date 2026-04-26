51e79220bc12b21534d098b452d5b049ade43167
Aham0803 <ahamgolash9340@gmail.com>
2026-04-26 11:59:20 +0530

    notes

---
+++ Python Learning/8_Loops.py
@@ -0,0 +1,15 @@
+items = (1 , 2 , 3 ,4 ,5)
+for i in (items):
+    print(f"Round: {i}")
+
+items = [1,2,3,4,'hi']
+for item in (items):
+    print(f"Round : {item}")
+
+items = "Python"
+for item in items:
+    print(f"Round : {item}")
+
+# Range(start,stop,step)
+for item in range(5):
+    print(f"Round : {item}")
