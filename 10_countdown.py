9d02bdb0ef3432a1b0db739deb3873989036b037
Aham0803 <ahamgolash9340@gmail.com>
2026-05-01 23:31:40 +0530

    notes

---
+++ 10_countdown.py
@@ -0,0 +1,20 @@
+#import time
+#def start_counting(seconds,delay = 1 ):
+#    for i in range(seconds,0,-1):
+#        print("counter is at:" , i)
+#       time.sleep(delay)
+#   print("counter delay")
+
+#num = int(input("enter the time of counter "))
+#start_counting(num)
+
+
+#name = "Gaurav"
+#for i in name:
+#    print(i)
+
+
+for i in range(1,6):
+    for j in range(1 , 6):
+        print(i)
+
